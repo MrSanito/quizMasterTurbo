5f11060bf66480325003413afb4477bb67707538
MrSanito <1988krishnani@gmail.com>
2026-01-11 02:43:49 +0530

    fixed cors issue

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -22,7 +22,10 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://quiz-master-turbo-quiz-master.vercel.app/",
+    ],
     credentials: true,
   })
 );
