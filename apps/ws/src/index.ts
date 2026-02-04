ae91deb5c81fcde33e3536d32c6a177559a8e733
MrSanito <1988krishnani@gmail.com>
2026-02-04 10:34:08 +0530

    used express instead of http

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,11 +1,15 @@
 import http from "http";
- import "./config.js"
+import express from "express";
+import "./config.js";
 import { setupSocket } from "./socket.js";
 
-const server = http.createServer();
+const app = express(); // 🔥 attach express
+const server = http.createServer(app); // use SAME server
 
 setupSocket(server);
 
-server.listen(3002, () => {
-  console.log("🚀 WS Server running on port 3002");
+const PORT = process.env.PORT || 3002; // Render provides PORT
+
+server.listen(PORT, () => {
+  console.log(`🚀 WS Server running on port ${PORT}`);
 });
