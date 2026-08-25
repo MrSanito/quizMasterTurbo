1e41a9df618b5292baf341ddce0c06142a9110ee
unknown <vishalni2004@gmail.com>
2026-08-25 22:16:18 +0530

    fix top level env access and logger for edge runtime compatibility

--- apps/hono/src/configs/resend.ts
+++ apps/hono/src/configs/resend.ts
@@ -1,7 +1,5 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export const sendResendEmail = async ({
 	to,
 	subject,
@@ -11,6 +9,7 @@ export const sendResendEmail = async ({
 	subject: string;
 	html: string;
 }) => {
+	const resend = new Resend(process.env.RESEND_API_KEY || "dummy_key");
 	try {
 		const response = await resend.emails.send({
 			from: process.env.RESEND_FROM_EMAIL || "onboarding@resend.dev",
--- apps/hono/src/configs/sendEmail.ts
+++ apps/hono/src/configs/sendEmail.ts
@@ -1,16 +1,16 @@
 import { createTransport } from "nodemailer";
 
-const transporter = createTransport({
-	host: "smtp.gmail.com",
-	port: 587,
-	secure: false,
-	auth: {
-		user: process.env.STMP_USER,
-		pass: process.env.STMP_PASSWORD,
-	},
-});
-
 export const sendMail = async (to: string, subject: string, html: string) => {
+	const transporter = createTransport({
+		host: "smtp.gmail.com",
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.STMP_USER,
+			pass: process.env.STMP_PASSWORD,
+		},
+	});
+
 	const res = await transporter.sendMail({
 		from: "[EMAIL_ADDRESS]",
 		to,
